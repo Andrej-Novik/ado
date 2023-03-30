4b2abffe2cdbea6afc0cd07d84ef7c386d3ca8c9
Andrej Novik <andrejnovikweb@gmail.com>
2023-03-31 01:18:58 +0300

    added cantainer

--- src/components/common/CartStatus.vue
+++ src/components/common/CartStatus.vue
@@ -11,6 +11,8 @@ export default {
     status: {
       type: String,
       default: "",
+      validator: (value) =>
+        ["Оплачен", "Отложен", "Ожидает оплаты"].includes(value),
     },
   },
   computed: {
