4f29bcd6719bd9083645e0cf898c8ac509bb524d
Andrej Novik <andrejnovikweb@gmail.com>
2023-03-31 00:29:09 +0300

    added call button

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,3 +1,14 @@
 <template>
-  <div></div>
+  <div>
+    <CallButton :color="'red'" :icon="'./icons/red-call.svg'" />
+  </div>
 </template>
+
+<script>
+import CallButton from "@/components/common/CallButton.vue";
+export default {
+  components: {
+    CallButton,
+  },
+};
+</script>
